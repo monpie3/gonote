0ee1770fc84207cc1d856f16816161e6444c328c
Monika <dotyk_zla@o2.pl>
2021-04-18 22:58:40 +0200

    fetching notes from the db and redefining GET endpoints

--- public/api.php
+++ public/api.php
@@ -5,32 +5,40 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+class NotesDB extends SQLite3 {
+    function __construct() {
+        $this->open('../notes.db');
+    }
+}
+$db = new NotesDB();
+
 $app = AppFactory::create();
 
 $app->get(
     '/api/notes',
-    function (Request $request, Response $response, array $args) {
-        $notes = [
-            ['id' => 1, 'name' => 'Spotkanie o 17:00', 'date' => '2021-03-13'],
-            ['id' => 2, 'name' => 'Lekcje o 8:00', 'date' => '2021-03-15'],
-            ['id' => 3, 'name' => 'Girls go IT!', 'date' => '2021-03-18'],
-        ];
-        $payload = json_encode($notes);
-        $response->getBody()->write($payload);
-        return $response->withHeader('Content-Type', 'application/json');
-    }
-   );
-
-   $app->get(
-    '/api/notes/{noteId}',
-    function (Request $request, Response $response, array $args) {
-        $noteId = $args["noteId"];
-        $note =  ['id' => $noteId, 'name' => "Spotkanie o $noteId:00", 'date' => date('Y-m-d')];
-        $payload = json_encode($note);
+    function (Request $request, Response $response, array $args) use($db) {
+        $notes = [];
+        $result = $db->query('SELECT * FROM note');
+        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            $notes[] = $row;
+        }
+        $payload = json_encode($notes, JSON_UNESCAPED_UNICODE);
         $response->getBody()->write($payload);
         return $response->withHeader('Content-Type', 'application/json');
     }
 );
 
+   $app->get(
+    '/api/notes/{noteId}',
+    function (Request $request, Response $response, array $args) use($db) {
+        $note = $db->query("SELECT * FROM note WHERE id = $args[noteId]")->fetchArray(SQLITE3_ASSOC);
+        if ($note) {
+            $response->getBody()->write(json_encode($note, JSON_UNESCAPED_UNICODE));
+            return $response->withHeader('Content-Type', 'application/json');
+        }
+        $response->getBody()->write(json_encode(['message' => 'Nie ma takiej notatki']));
+        return $response->withStatus(404)->withHeader('Content-Type', 'application/json');
+    }
+);
 
 $app->run();
